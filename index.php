64877198893695011153277535389e25b6b6787d
swarup755 <166317776+swarup755@users.noreply.github.com>
2024-08-30 00:33:16 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Page title</title>
+</head>
+<body>
+    hi babay
+    <?php include'header.html'
+   ?>
+</body>
+</html>
